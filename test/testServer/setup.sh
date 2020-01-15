4080cc8006fc88fc440bf9a0bebdbac897ea61d9
Sergey Grosman <43882757+Sergey-Grosman@users.noreply.github.com>
2020-01-15 17:22:07 +0100

    Update setup.sh

--- test/testServer/setup.sh
+++ test/testServer/setup.sh
@@ -2,3 +2,6 @@
 # file: setup.sh
 # setup minimal ubuntu installation as a test server
 apt-get update && apt-get upgrade -y
+docker pull jenkins
+docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 \
+  --restart unless-stopped --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"' jenkins/jenkins
