aa14c3a07767726441e0887beb3f1f7fc174222f
Sergey Grosman <43882757+Sergey-Grosman@users.noreply.github.com>
2020-01-16 14:02:23 +0100

    Update setup.sh

--- test/testServer/setup.sh
+++ test/testServer/setup.sh
@@ -3,10 +3,10 @@
 # setup minimal ubuntu installation as a test server
 apt update && apt upgrade -y
 apt install -y docker.io
-docker pull jenkins
-docker volume create jenkins_home
+docker pull jenkins/jenkins
+#docker volume create jenkins_home
 docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 \
-  --restart unless-stopped --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"' jenkins
+  --restart unless-stopped --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"' jenkins/jenkins
 echo "Waiting for jenkins-admin password..."
 until [ -f /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword ]
 do
