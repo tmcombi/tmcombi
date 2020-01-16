75c6868b9e1baf3174d111deda64eee49d6ba23a
Sergey Grosman <43882757+Sergey-Grosman@users.noreply.github.com>
2020-01-16 15:32:51 +0100

    Update setup.sh

--- test/testServer/setup.sh
+++ test/testServer/setup.sh
@@ -9,8 +9,7 @@ apt install -y docker.io
 
 ## jenkins
 docker pull jenkins/jenkins
-docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 \
-  --restart unless-stopped --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"' jenkins/jenkins
+docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --name jenkins --restart unless-stopped --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"' jenkins/jenkins
 echo "Waiting for initial jenkins-admin password..."
 until [ -f /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword ]
 do
@@ -23,4 +22,5 @@ sleep 1
 
 ## portainer
 docker pull portainer/portainer
+docker run -d -v portainer_data:/data -p 9000:9000 -p 8000:8000 --name portainer --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
 
