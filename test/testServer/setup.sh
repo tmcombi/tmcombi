61e71ccccafd9c3868dc00dca8b1f557623ebc0a
Sergey Grosman <43882757+Sergey-Grosman@users.noreply.github.com>
2020-01-16 09:15:26 +0100

    Update setup.sh

--- test/testServer/setup.sh
+++ test/testServer/setup.sh
@@ -6,3 +6,11 @@ docker pull jenkins
 docker volume create jenkins_home
 docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 \
   --restart unless-stopped --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"' jenkins
+until [ -f /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword ]
+do
+     sleep 1
+done
+echo "initial jenkins-admin passowrd:"
+sleep 1
+cat /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword
+sleep 1
