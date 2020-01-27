67d6981382b0ec16bc59dab4c5013770d6828204
Sergey Grosman <43882757+Sergey-Grosman@users.noreply.github.com>
2020-01-27 23:32:09 +0100

    Update setup.sh

--- test/testServer/setup.sh
+++ test/testServer/setup.sh
@@ -6,13 +6,16 @@
 apt update && apt upgrade -y
 apt install -y docker.io
 
+## docker network
+docker network inspect testServerNetwork
+
 ## nginx
 docker pull nginx
-docker run -d -v nginx_html:/usr/share/nginx/html:ro -v nginx_conf:/etc/nginx:ro -p 80:80 -p 443:443 --restart unless-stopped nginx
+docker run -d -v nginx_html:/usr/share/nginx/html:ro -v nginx_conf:/etc/nginx:ro -p 80:80 -p 443:443 --restart unless-stopped --net=testServerNetwork nginx
 
 ## jenkins
 docker pull jenkins/jenkins:lts
-docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --restart unless-stopped --env JENKINS_OPTS="--prefix=/jenkins" --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"' jenkins/jenkins:lts
+docker run -d -v jenkins_home:/var/jenkins_home --restart unless-stopped --env JENKINS_OPTS="--prefix=/jenkins" --env 'JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts;"'  --net=testServerNetwork  --network-alias jenkins jenkins/jenkins:lts
 echo "Waiting for initial jenkins-admin password..."
 until [ -f /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword ]
 do
