50dfa759309d2831a82dd72267d9b82a3f2c0e35
Sergey Grosman <43882757+Sergey-Grosman@users.noreply.github.com>
2020-02-17 00:26:57 +0100

    Update init.sh

--- test/testServer/init.sh
+++ test/testServer/init.sh
@@ -84,3 +84,14 @@ echo
 
 echo "### Reloading nginx ..."
 docker-compose exec nginx nginx -s reload
+
+echo "Waiting for initial jenkins-admin password..."
+until [ -f /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword ]
+do
+     sleep 1
+done
+echo "initial jenkins-admin password:"
+sleep 1
+cat /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword
+sleep 1
+
