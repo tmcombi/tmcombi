c76df5f3792435e23078d1f0ab839b84ae69d586
Sergey Grosman <srg.grosman@gmail.com>
2020-05-16 11:04:48 +0200

    update

--- test/testServer/init_local.sh
+++ test/testServer/init_local.sh
@@ -7,6 +7,3 @@ sudo ./init_shared.sh
 echo "### Starting nginx ..."
 sudo docker-compose up --force-recreate -d nginx
 echo
-
-# nice link:
-# https://devopscube.com/jenkins-master-build-slaves-docker-container/
