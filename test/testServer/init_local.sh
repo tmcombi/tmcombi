1e84316620fb99162c314bb6f7b9904db54f4138
Sergey Grosman <srg.grosman@gmail.com>
2020-06-01 00:00:25 +0200

    update

--- test/testServer/init_local.sh
+++ test/testServer/init_local.sh
@@ -2,6 +2,10 @@
 
 sudo ./init_shared.sh
 
+if ! [ -x "$(command -v ifconfig)" ]; then
+    apt update && apt upgrade -y && apt install -y net-tools
+fi
+
 \cp ./data/nginx/conf.d.local/app.conf ./data/nginx/conf.d/app.conf
 
 echo "### Starting nginx ..."
