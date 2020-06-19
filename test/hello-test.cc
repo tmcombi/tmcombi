363233c0e4bffbe54515eef82b0a07824ce9935b
Sergey Grosman <srg.grosman@gmail.com>
2020-06-19 23:07:13 +0200

    update

--- test/hello-test.cc
+++ test/hello-test.cc
@@ -1,7 +1,5 @@
 #include "gtest/gtest.h"
 #include "main/hello-greet.h"
-//#define BOOST_TEST_MODULE MyTest
-//#include <boost/test/unit_test.hpp>
 
 TEST(HelloTest, GetGreet) {
   EXPECT_EQ(get_greet("Bazel"), "Hello Bazel");
@@ -13,7 +11,6 @@ TEST(HelloTest, GetGreet1) {
   EXPECT_EQ(get_greet("Bazel1"), "Hello Bazel1");
   EXPECT_EQ(4, 4);
   EXPECT_EQ(3, 3);
-  EXPECT_EQ(3, 2);
 }
 
 TEST(HelloTest1, GetGreet1) {
