6b8333ffd5553d7091255a77779df3d20476bb6d
Your Name <you@example.com>
2020-08-23 01:58:00 +0200

    update

--- python/data.py
+++ python/data.py
@@ -7,44 +7,82 @@ from names import Names
 class Transform:
     def __init__(self, names):
         self.names = names
-        self.rule_forward = {}
-        self.rule_reverse = {}
-        self.linear_forward_a = {}
-        self.linear_reverse_a = {}
-        self.linear_forward_b = {}
-        self.linear_reverse_b = {}
+        self.rule = {}
         self.forward = {}
+        self.backward = {}
 
     def monotone_from_stat(self, stat):
         for feature_name in self.names.feature_list:
             feature = self.names.feature[feature_name]
+            self.forward[feature_name] = {}
+            self.backward[feature_name] = {}
             if feature.type == 'continuous':
-                self.rule_forward[feature_name] = 'linear'
-                self.rule_reverse[feature_name] = 'linear'
-                self.linear_forward_b[feature_name] = 0
-                self.linear_reverse_b[feature_name] = 0
+                self.rule[feature_name] = 'linear'
+                self.forward[feature_name]['b'] = 0
+                self.backward[feature_name]['b'] = 0
                 if stat.corr_with_target[feature_name] >= 0:
-                    self.linear_forward_a[feature_name] = 1
-                    self.linear_reverse_a[feature_name] = 1
+                    self.forward[feature_name]['a'] = 1
+                    self.backward[feature_name]['a'] = 1
                 else:
-                    self.linear_forward_a[feature_name] = -1
-                    self.linear_reverse_a[feature_name] = -1
+                    self.forward[feature_name]['a'] = -1
+                    self.backward[feature_name]['a'] = -1
             if feature.type == 'categorical':
-                self.rule_forward[feature_name] = 'cat2continuous'
-                self.rule_reverse[feature_name] = 'continuous2cat'
-                self.forward[feature_name] = {}
+                self.rule[feature_name] = 'cat2continuous'
                 for val in self.names.feature[feature_name].values:
                     self.forward[feature_name][val] = stat.category_goodness[feature_name][val]
-                foo = 1
+                    self.backward[feature_name][stat.category_goodness[feature_name][val]] = val
 
+    def transform_line(self, line):
+        line = re.sub(r"\n", "", line)
+        line = re.sub(r"[ ]*,[ ]*", ",", line)
+        line = re.sub(r"[ ]*\|.*", "", line)
+        line = re.sub(r"[\. ]*$", "", line)
+        if line == '':
+            return line
+        data = re.split(",", line)
+        data_out = []
+        for feature_name, val in zip(self.names.feature_list, data):
+            if val == '?':
+                data_out.append('?')
+                continue
+            if self.rule[feature_name] == 'linear':
+                val_out = self.forward[feature_name]['a']*float(val) + self.forward[feature_name]['b']
+                if val_out == round(val_out):
+                    val_out = round(val_out)
+                data_out.append(str(val_out))
+                continue
+            if self.rule[feature_name] == 'cat2continuous':
+                val_out = self.forward[feature_name][val]
+                if val_out == round(val_out):
+                    val_out = round(val_out)
+                data_out.append(str(val_out))
+                continue
+            print('Transformation object does not have a rule for feature ' + feature_name)
+            exit(-1)
+        line_out = ','.join(data_out)
+        return line_out
 
+    def transform_file(self, file, file_out):
+        fp = open(file, 'r')
+        fp_out = open(file_out, 'w')
+        line = fp.readline()
+        while line:
+            line_out = self.transform_line(line)
+            line = fp.readline()
+            fp_out.write(line_out + '\n')
+        fp.close()
+        fp_out.close()
 
-    def forward(self, vector):
-        return 0
-
-    def inverse(self):
-        return 0
-
+    def print_names(self, out_file):
+        fp = open(out_file, 'w')
+        fp.write(self.names.target_feature + '.' + '\n')
+        fp.write('\n')
+        for feature_name in self.names.feature_list:
+            if feature_name == self.names.target_feature:
+                fp.write(feature_name + ': 0,1.' + '\n')
+            else:
+                fp.write(feature_name + ': continuous.' + '\n')
+        fp.close()
 
 class Statistics:
     def __init__(self, names):
@@ -103,8 +141,7 @@ class Statistics:
         fp = open(file, 'r')
         line = fp.readline()
         while line:
-            line = self.process_line(line, names, map_to_numeric)
-            print(line)
+            self.process_line(line, names, map_to_numeric)
             line = fp.readline()
         fp.close()
         for feature_name in names.feature_list:
@@ -133,17 +170,17 @@ class TestData(unittest.TestCase):
     def test_names_real_file(self):
         names = Names().from_file('adult.names')
         self.assertEqual(names.size(), 15)
-        # names = Names().from_file('../data/tmc_paper/tmc_paper.names')
         stat = Statistics(names)
         map_to_numeric = {}
         map_to_numeric[names.target_feature] = {}
         map_to_numeric[names.target_feature][names.feature[names.target_feature].values[0]] = 1
         map_to_numeric[names.target_feature][names.feature[names.target_feature].values[1]] = 0
         stat.process_file('adult.data', names, map_to_numeric)
-        # stat.process_file('adult.test', names, map_to_numeric)
-        # stat.process_file('../data/tmc_paper/tmc_paper.data', names, map_to_numeric)
         trans = Transform(names)
         trans.monotone_from_stat(stat)
+        trans.print_names('adult_transformed.names')
+        trans.transform_file('adult.data', 'adult_transformed.data')
+        trans.transform_file('adult.test', 'adult_transformed.test')
 
 
 if __name__ == "__main__":
