168e2b4d588254caaac30464c33f44cdb7125e26
Sergey Grosman <srg.grosman@gmail.com>
2020-09-06 23:38:10 +0200

    refactoring

--- lib/feature_vector.cc
+++ lib/feature_vector.cc
@@ -5,7 +5,28 @@
 
 #include "sample.h"
 
-BOOST_AUTO_TEST_CASE( feature_vector )
+BOOST_AUTO_TEST_CASE( basic_checks )
+{
+    // seven ways to detect and report the same error:
+    BOOST_CHECK( add( 2,2 ) == 4 );        // #1 continues on error
+
+    BOOST_REQUIRE( add( 2,2 ) == 4 );      // #2 throws on error
+
+    if( add( 2,2 ) != 4 )
+        BOOST_ERROR( "Ouch..." );            // #3 continues on error
+
+    if( add( 2,2 ) != 4 )
+        BOOST_FAIL( "Ouch..." );             // #4 throws on error
+
+    if( add( 2,2 ) != 4 ) throw "Ouch..."; // #5 throws on error
+
+    BOOST_CHECK_MESSAGE( add( 2,2 ) == 4,  // #6 continues on error
+                         "add(..) result: " << add( 2,2 ) );
+
+    BOOST_CHECK_EQUAL( add( 2,2 ), 4 );	  // #7 continues on error
+}
+
+BOOST_AUTO_TEST_CASE( test_data )
 {
     // seven ways to detect and report the same error:
     BOOST_CHECK( add( 2,2 ) == 4 );        // #1 continues on error
