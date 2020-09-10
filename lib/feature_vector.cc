e072e20511c3a197923eace9771b1c2e1613cc93
Sergey Grosman <srg.grosman@gmail.com>
2020-09-10 22:35:21 +0200

    update

--- lib/feature_vector.cc
+++ lib/feature_vector.cc
@@ -18,38 +18,68 @@ BOOST_AUTO_TEST_CASE( basic_checks )
     BOOST_CHECK_EQUAL( fv[1],  22 );
 }
 
-bool is_critical( int code ) { return code < 0; }
+bool is_critical(const std::exception& ex ) { return true; }
 
 BOOST_AUTO_TEST_CASE( check_string_buffer_input )
 {
-    FeatureVector fv("11,22,33,44,55,66,77",{3,2},6,"55","6g");
+    FeatureVector
+    fv("11,22,33,44,55,66,77",{3,2},5,"foo","66");
     BOOST_TEST_MESSAGE("Testing feature vector: " << fv);
     BOOST_CHECK_EQUAL(  fv.get_dim(), 2 );
-    BOOST_CHECK_EQUAL(  fv[0], 33 );
-    BOOST_CHECK_EQUAL(  fv[1], 22 );
+    BOOST_CHECK_EQUAL(  fv[0], 44 );
+    BOOST_CHECK_EQUAL(  fv[1], 33 );
     BOOST_CHECK_EQUAL(  fv.get_weight_positives(), 1 );
     BOOST_CHECK_EQUAL(  fv.get_weight_negatives(), 0 );
 
-    BOOST_TEST_MESSAGE("Non existing element: " << fv[234]);
-    BOOST_CHECK_EXCEPTION( fv[234], int, is_critical);
-    BOOST_CHECK_EXCEPTION( throw(-234), int, is_critical);
+    FeatureVector
+            fv1("11,22,33,44,55,66,77",{3,2},5,"foo","66", 6);
+    BOOST_CHECK_EQUAL(  fv1.get_weight_positives(), 77 );
+
+    FeatureVector
+            fv2("11,22,33,44,55,foo,77",{3,2},5,"foo","66", 6);
+    BOOST_CHECK_EQUAL(  fv2.get_weight_negatives(), 77 );
  }
- /*
-// seven ways to detect and report the same error:
-BOOST_CHECK( add( 2,2 ) == 4 );        // #1 continues on error
 
-BOOST_REQUIRE( add( 2,2 ) == 4 );      // #2 throws on error
+BOOST_AUTO_TEST_CASE( check_string_buffer_input_exceptions ) {
+    //operator[] out of range
+    FeatureVector fv("11,22,33,44,55,66,77",{3,2},5,"foo","66");
+    BOOST_CHECK_EXCEPTION( fv[234], std::out_of_range, is_critical);
 
-if( add( 2,2 ) != 4 )
-BOOST_ERROR( "Ouch..." );            // #3 continues on error
+    //class not known
+    BOOST_CHECK_EXCEPTION(
+            FeatureVector("11,22,33,44,55,66,77",{3,2},5,"foo","bar"), std::invalid_argument, is_critical);
 
-if( add( 2,2 ) != 4 )
-BOOST_FAIL( "Ouch..." );             // #4 throws on error
+    //weight negative
+    //weight not numeric
+    //feature not numeric
 
-if( add( 2,2 ) != 4 ) throw "Ouch..."; // #5 throws on error
+    //target_feature_index out of range
+    BOOST_CHECK_EXCEPTION(
+            FeatureVector("11,22,33,44,55,66,77",{3,2},555,"foo","bar"), std::out_of_range, is_critical);
+    //feature_index out of range
+    BOOST_CHECK_EXCEPTION(
+            FeatureVector("11,22,33,44,55,bar,77",{333,2},5,"foo","bar"), std::out_of_range, is_critical);
+    //weight_index out of range
+    BOOST_CHECK_EXCEPTION(
+            FeatureVector("11,22,33,44,55,66,77",{3,2},5,"foo","bar",1024), std::out_of_range, is_critical);
+}
 
-BOOST_CHECK_MESSAGE( add( 2,2 ) == 4,  // #6 continues on error
-                     "add(..) result: " << add( 2,2 ) );
+    /*
+   // seven ways to detect and report the same error:
+   BOOST_CHECK( add( 2,2 ) == 4 );        // #1 continues on error
 
-BOOST_CHECK_EQUAL( add( 2,2 ), 4 );	  // #7 continues on error
-*/
+   BOOST_REQUIRE( add( 2,2 ) == 4 );      // #2 throws on error
+
+   if( add( 2,2 ) != 4 )
+   BOOST_ERROR( "Ouch..." );            // #3 continues on error
+
+   if( add( 2,2 ) != 4 )
+   BOOST_FAIL( "Ouch..." );             // #4 throws on error
+
+   if( add( 2,2 ) != 4 ) throw "Ouch..."; // #5 throws on error
+
+   BOOST_CHECK_MESSAGE( add( 2,2 ) == 4,  // #6 continues on error
+                        "add(..) result: " << add( 2,2 ) );
+
+   BOOST_CHECK_EQUAL( add( 2,2 ), 4 );	  // #7 continues on error
+   */
--- lib/feature_vector.h
+++ lib/feature_vector.h
@@ -2,12 +2,13 @@
 #define LIB_FEATURE_VECTOR_H_
 
 #include <vector>
+#include <boost/algorithm/string.hpp>
 
 class FeatureVector {
 public:
     explicit FeatureVector(std::vector<double>);
     explicit FeatureVector(const std::string &, const std::vector<unsigned int> &,
-            unsigned int, const std::string &, const std::string &);
+            unsigned int, const std::string &, const std::string &, int);
     ~FeatureVector();
 
     unsigned int get_dim() const;
@@ -21,63 +22,80 @@ public:
     friend std::ostream& operator<< (std::ostream&, const FeatureVector&);
 
 private:
-    double weight_negatives{};
-    double weight_positives{};
-    std::vector<double> data;
+    double weight_negatives_;
+    double weight_positives_;
+    std::vector<double> data_;
 };
 
 unsigned int FeatureVector::get_dim() const {
-    return data.size();
+    return data_.size();
 }
 
 double FeatureVector::get_weight_negatives() const {
-    return weight_negatives;
+    return weight_negatives_;
 }
 
 double FeatureVector::get_weight_positives() const {
-    return weight_positives;
+    return weight_positives_;
 }
 
 FeatureVector & FeatureVector::inc_weight_negatives(const double d) {
-    weight_negatives += d;
+    weight_negatives_ += d;
     return *this;
 }
 
 FeatureVector & FeatureVector::inc_weight_positives(const double d) {
-    weight_positives += d;
+    weight_positives_ += d;
     return *this;
 }
 
 FeatureVector::FeatureVector(std::vector<double> data):
-weight_negatives {0},
-weight_positives {0},
-data {std::move(data)}
+weight_negatives_ {0},
+weight_positives_ {0},
+data_ {std::move(data)}
 {}
 
 double FeatureVector::operator[](const unsigned int i) const {
-    return data[i];
+    if (i>=data_.size()) throw std::out_of_range("Index must not exceed the size!");
+    return data_[i];
 }
 
 std::ostream & operator<<(std::ostream & stream, const FeatureVector & fv) {
     stream << "[data:{";
-    if (!fv.data.empty()) {
-        stream << fv.data[0];
+    if (!fv.data_.empty()) {
+        stream << fv.data_[0];
     }
-    for (unsigned int i = 1; i < fv.data.size(); ++i) {
-        stream << ',' << fv.data[i];
+    for (unsigned int i = 1; i < fv.data_.size(); ++i) {
+        stream << ',' << fv.data_[i];
     }
-    stream << "},w_neg:" << fv.weight_negatives << ",w_pos:" << fv.weight_positives << ']';
+    stream << "},w_neg:" << fv.weight_negatives_ << ",w_pos:" << fv.weight_positives_ << ']';
     return stream;
 }
 
-FeatureVector::FeatureVector(const std::string & data, const std::vector<unsigned int> & selected_features,
-                             unsigned int target_feature, const std::string & negatives_label,
-                             const std::string & positives_label):
-weight_negatives {0},
-weight_positives {0},
-data(selected_features.size())
+FeatureVector::FeatureVector(const std::string & data, const std::vector<unsigned int> & selected_feature_index,
+                             unsigned int target_feature_index, const std::string & negatives_label,
+                             const std::string & positives_label, const int weight_index = -1):
+weight_negatives_ {0},
+weight_positives_ {0},
+data_(selected_feature_index.size())
 {
+    std::vector<std::string> str_vector;
+    boost::split(str_vector, data, boost::is_any_of(","));
+    for (int i = 0; i < selected_feature_index.size(); ++i) {
+        data_[i] = std::stod(str_vector[selected_feature_index[i]]);
+    }
 
+    unsigned int weight = 1;
+    if (weight_index >= 0) {
+        weight = std::stoi(str_vector[weight_index]);
+    }
+
+    if (str_vector[target_feature_index] == negatives_label)
+        weight_negatives_ = weight;
+    else if (str_vector[target_feature_index] == positives_label)
+        weight_positives_ = weight;
+    else
+        throw std::invalid_argument("Class label is unknown!");
 }
 
 
