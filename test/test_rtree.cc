b1466d15d742be5327a24f2bcf33be5882eb519b
Sergey Grosman <srg.grosman@gmail.com>
2020-10-05 00:21:07 +0200

    refactoring

--- test/test_rtree.cc
+++ test/test_rtree.cc
@@ -57,54 +57,86 @@ inline bool MySearchCallback(unsigned int id)
     return false;
 }
 
+class GenerateBorder {
+public:
+    GenerateBorder(Point * border, const unsigned int n) {
+        std::cout << "Generating border with number of points=" << n << std::endl;
+        for(unsigned int i=0; i<n; i++) {
+            do {
+                border[i]=Point(drand(), drand(), drand(), drand());
+                const double squeeze_factor = 1 + 1.407777778*drand();
+                const double sum = border[i].x + border[i].y + border[i].z + border[i].t;
+                border[i].x = std::min<double>(squeeze_factor*border[i].x/sum,1);
+                border[i].y = std::min<double>(squeeze_factor*border[i].y/sum,1);
+                border[i].z = std::min<double>(squeeze_factor*border[i].z/sum,1);
+                border[i].t = std::min<double>(squeeze_factor*border[i].t/sum,1);
+            } while (!incomparable(border,border[i],i));
+        }
+        std::cout << "Border created" << std::endl;
+    }
+};
+
+class GenerateRtree {
+public:
+    GenerateRtree(RTree<unsigned int,double,4> & tree, Point * border, const unsigned int n) {
+        for(unsigned int i=0; i<n; i++) {
+            const double p[4]={border[i].x, border[i].y, border[i].z, border[i].t};
+            tree.Insert(p,p,i);
+        }
+    }
+};
+
+class GenerateData2Test{
+public:
+    GenerateData2Test(Point * point2test, double double2test[][4], const unsigned int n) {
+        for(unsigned int i=0; i<n; i++) {
+            point2test[i] = {drand(), drand(), drand(), drand()};
+            double2test[i][0] = point2test[i].x;
+            double2test[i][1] = point2test[i].y;
+            double2test[i][2] = point2test[i].z;
+            double2test[i][3] = point2test[i].t;
+        }
+    }
+};
+
+const double p0[4]={0,0,0,0};
+const unsigned int NUM_BOUNDARY_POINTS = 4096;
+const unsigned int NUM_TEST_OBJECTS = 500000;
+static Point border[NUM_BOUNDARY_POINTS];
+static GenerateBorder generateBorder(border, NUM_BOUNDARY_POINTS);
+const double p10[4]={border[10].x,border[10].y,border[10].z,border[10].t};
+
+RTree<unsigned int, double, 4> tree_1k, tree_2k, tree_4k;
+GenerateRtree generateRtree_1k(tree_1k, border, NUM_BOUNDARY_POINTS/4);
+GenerateRtree generateRtree_2k(tree_2k, border, NUM_BOUNDARY_POINTS/2);
+GenerateRtree generateRtree_4k(tree_4k, border, NUM_BOUNDARY_POINTS);
+
+static Point point2test[NUM_TEST_OBJECTS];
+static double double2test[NUM_TEST_OBJECTS][4];
+GenerateData2Test generateData2Test(point2test, double2test, NUM_TEST_OBJECTS);
+
 BOOST_AUTO_TEST_CASE( test_rtree_consistency ) {
-    const int NUM_BOUNDARY_POINTS = 4096;
-    const int NUM_TEST_OBJECTS = 500000;
 
-    std::cout << "NUM_BOUNDARY_POINTS="	<< NUM_BOUNDARY_POINTS << std::endl;
-
-    Point border[NUM_BOUNDARY_POINTS];
-    for(unsigned int i=0; i<NUM_BOUNDARY_POINTS; i++) {
-        do {
-            border[i]=Point(drand(), drand(), drand(), drand());
-            const double squeeze_factor = 1 + 1.407777778*drand();
-            const double sum = border[i].x + border[i].y + border[i].z + border[i].t;
-            border[i].x = std::min<double>(squeeze_factor*border[i].x/sum,1);
-            border[i].y = std::min<double>(squeeze_factor*border[i].y/sum,1);
-            border[i].z = std::min<double>(squeeze_factor*border[i].z/sum,1);
-            border[i].t = std::min<double>(squeeze_factor*border[i].t/sum,1);
-        } while (!incomparable(border,border[i],i));
-    }
-    std::cout << "boundary created" << std::endl;
-    RTree<unsigned int, double, 4> tree;
-    for(unsigned int i=0; i<NUM_BOUNDARY_POINTS/2; i++) {
-        const double p[4]={border[i].x, border[i].y, border[i].z, border[i].t};
-        tree.Insert(p,p,i);
-    }
+    BOOST_CHECK(point_above (border, border[10], NUM_BOUNDARY_POINTS/2) );
+    BOOST_CHECK_EQUAL(tree_2k.Search(p0, p10, MySearchCallback), 1 );
 
     unsigned int above_count = 0, below_count=0;
-    const double p0[4]={0,0,0,0};
-
-    //small test
-    const double p10[4]={border[10].x,border[10].y,border[10].z,border[10].t};
-    BOOST_CHECK(point_above (border, border[10], NUM_BOUNDARY_POINTS/2) );
-    BOOST_CHECK_EQUAL(tree.Search(p0, p10, MySearchCallback), 1 );
-
-    std::cout << "NUM_TEST_OBJECTS="	<< NUM_TEST_OBJECTS << std::endl;
+    BOOST_TEST_MESSAGE("Input objects to test="	<< NUM_TEST_OBJECTS);
     for(unsigned int i=0; i<NUM_TEST_OBJECTS; i++) {
-        const Point p(drand(), drand(), drand(), drand());
-        const double pd[4]={p.x,p.y,p.z,p.t};
-        const bool above_slow = point_above (border, p, NUM_BOUNDARY_POINTS/2);
-        const unsigned int nhits = tree.Search(p0, pd, MySearchCallback);
-        if (nhits > 0 && !above_slow)
+        const bool above_slow = point_above (border, point2test[i], NUM_BOUNDARY_POINTS/2);
+        const unsigned int nhits = tree_2k.Search(p0, double2test[i], MySearchCallback);
+        if (nhits > 0 && !above_slow) {
             BOOST_FAIL("RTree reports above, but not the point_above");
-        if (nhits == 0 && above_slow)
+            exit(-1);
+        }
+        if (nhits == 0 && above_slow) {
             BOOST_FAIL("point_above reports true, but not the RTree");
+            exit(-1);
+        }
         if (above_slow)
             above_count++;
         else
             below_count++;
     }
-    std::cout << "below_count="	<< below_count << ", above_count=" << above_count << std::endl;
-
+    BOOST_TEST_MESSAGE("below_count="	<< below_count << ", above_count=" << above_count);
 }
