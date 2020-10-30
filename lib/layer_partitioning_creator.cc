77f1f74c9ad3c74c7b94a6a1a2c7671bbad4e19f
Sergey Grosman <srg.grosman@gmail.com>
2020-10-30 14:02:20 +0100

    first version of optimized layer partiotioning creation

--- lib/layer_partitioning_creator.cc
+++ lib/layer_partitioning_creator.cc
@@ -46,6 +46,47 @@ BOOST_AUTO_TEST_CASE( layer_partitioning_creator_36points ) {
     std::shared_ptr<LayerPartitioningCreator> pLayerPartitioningCreator = std::make_shared<LayerPartitioningCreator>();
     pLayerPartitioningCreator->push_back(pSample);
     pLayerPartitioningCreator->optimize();
-    std::shared_ptr<LayerPartitioning> pLayerPartitioning(pLayerPartitioningCreator->get_layer_partitioning());
-    BOOST_CHECK_EQUAL(pLayerPartitioning->size(), 4);
+    std::shared_ptr<LayerPartitioning> pLD(pLayerPartitioningCreator->get_layer_partitioning());
+    BOOST_CHECK_EQUAL(pLD->size(), 4);
+
+
+    BOOST_CHECK_EQUAL(pLD->consistent(),true);
+    auto pLayer1 = *pLD->begin();
+    auto pLayer2 = *(pLD->begin() + 1);
+    auto pLayer3 = *(pLD->begin() + 2);
+    auto pLayer4 = *(pLD->begin() + 3);
+
+    BOOST_TEST_MESSAGE("First layer: " << *pLayer1);
+    BOOST_TEST_MESSAGE("Second layer: " << *pLayer2);
+    BOOST_TEST_MESSAGE("Third layer: " << *pLayer3);
+    BOOST_TEST_MESSAGE("Fourth layer: " << *pLayer4);
+
+    BOOST_CHECK_LE(*pLayer1, *pLayer2);
+    BOOST_CHECK_LE(*pLayer1, *pLayer3);
+    BOOST_CHECK_LE(*pLayer1, *pLayer4);
+    BOOST_CHECK_LE(*pLayer2, *pLayer3);
+    BOOST_CHECK_LE(*pLayer2, *pLayer4);
+    BOOST_CHECK_LE(*pLayer3, *pLayer4);
+    BOOST_CHECK_GE(*pLayer4, *pLayer3);
+    BOOST_CHECK_GE(*pLayer3, *pLayer2);
+    BOOST_CHECK_GE(*pLayer2, *pLayer1);
+
+    BOOST_CHECK_EQUAL(pLayer1->get_neg_pos_counts().second / pLayer1->get_neg_pos_counts().first, 1.0/5.0 );
+    BOOST_CHECK_EQUAL(pLayer2->get_neg_pos_counts().second / pLayer2->get_neg_pos_counts().first, 1.0/2.0 );
+    BOOST_CHECK_EQUAL(pLayer3->get_neg_pos_counts().second / pLayer3->get_neg_pos_counts().first, 2.0/1.0 );
+    BOOST_CHECK_EQUAL(pLayer4->get_neg_pos_counts().second / pLayer4->get_neg_pos_counts().first, 5.0/1.0 );
+
+    BOOST_CHECK_EQUAL(boost::num_vertices(*pLD->get_graph(pLayer1)), pLayer1->size());
+    BOOST_CHECK_EQUAL(boost::num_vertices(*pLD->get_graph(pLayer2)), pLayer2->size());
+    BOOST_CHECK_EQUAL(boost::num_vertices(*pLD->get_graph(pLayer3)), pLayer3->size());
+    BOOST_CHECK_EQUAL(boost::num_vertices(*pLD->get_graph(pLayer4)), pLayer4->size());
+
+    pLD->merge_two_layers(pLD->begin()+1);
+    BOOST_CHECK_EQUAL(pLD->consistent(),true);
+    pLD->merge_two_layers(pLD->begin()+1);
+    BOOST_CHECK_EQUAL(pLD->consistent(),true);
+    pLD->merge_two_layers(pLD->begin());
+    BOOST_CHECK_EQUAL(pLD->consistent(),true);
+
+    BOOST_CHECK_EQUAL(36, boost::num_vertices(*pLD->get_graph(*pLD->begin())));
 }
