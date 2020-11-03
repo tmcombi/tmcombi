1d58b64e14acdb023929882e4a9ad1667fc1b55c
Sergey Grosman <srg.grosman@gmail.com>
2020-11-03 22:01:27 +0100

    add evaluator implementation and test

--- lib/evaluator.cc
+++ lib/evaluator.cc
@@ -6,6 +6,10 @@
 #include "layer_partitioning.h"
 #include "sample_creator.h"
 
+#define PUSH(x,y,neg,pos,pFV,pS2E) pFV = std::make_shared<FeatureVector>(std::vector<double>({x,y})); \
+(*pFV).inc_weight_negatives(neg).inc_weight_positives(pos);                                  \
+pS2E->push(pFV);
+
 
 BOOST_AUTO_TEST_CASE( evaluator_basics ) {
     const std::string names_file("data/4layers_36points/4layers_36points.names");
@@ -42,12 +46,57 @@ BOOST_AUTO_TEST_CASE( evaluator_basics ) {
     std::shared_ptr<BorderSystemCreator> pBSC = std::make_shared<BorderSystemCreator>();
     std::shared_ptr<BorderSystem> pBS = pBSC->from_layer_partitioning(pLD);
     BOOST_CHECK_EQUAL(pBS.use_count(), 1);
-    std::vector<double> p;  std::pair<int, int> borders; std::pair<double, double> conf;
-    p = {10,2}; borders={0,0}; BOOST_CHECK(pBS->containing_borders(p) == borders);
+    std::vector<double> p;  std::pair<double, double> conf;
+
     p = {10,2}; conf = {1.0/6.0,1.0/6.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {16,1}; conf = {1.0/3.0,1.0/3.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {7,10}; conf = {2.0/3.0,2.0/3.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {11,10}; conf = {5.0/6.0,5.0/6.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {4,6}; conf = {0,1.0/6.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {7,13}; conf = {5.0/6.0,1}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {14.5,0.5}; conf = {0,1.0/3.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {-1,16}; conf = {0,5.0/6.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {-1,18}; conf = {0,1}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {12.5,1.5}; conf = {1.0/6.0,1.0/3.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {4,8}; conf = {1.0/6.0,2.0/3.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {3,13}; conf = {1.0/6.0,5.0/6.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {1,18};  conf = {1.0/6.0,1}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {15,5.5}; conf = {1.0/3.0,5.0/6.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {18,1}; conf = {1.0/3.0,1}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+    p = {8,12}; conf = {2.0/3.0,5.0/6.0}; BOOST_CHECK(pBS->confidence_interval(p) == conf);
+
+    std::shared_ptr<FeatureVector> pFV(nullptr);
+    auto pS2E = std::make_shared<Sample>(2);
+    PUSH(10,2,1,4,pFV,pS2E); /// {1/6,1/6} TP=0 FP=0 FN=4 TN=1 Confl=1*4/2=2
+    PUSH(16,1,2,3,pFV,pS2E); /// {1/3,1/3} TP=0 FP=0 FN=3 TN=2 Confl=2*3/2+2*(4)+3*(0)=11
+    PUSH(7,10,3,2,pFV,pS2E); /// {2/3,2/3} TP=2 FP=3 FN=0 TN=0 Confl=3*2/2+3*(4+3)+2*(0)=24
+    PUSH(11,10,4,1,pFV,pS2E); /// {5/6,5/6} TP=1 FP=4 FN=0 TN=0 Confl=4*1/2+4*(4+3+2)+1*(0)=38
+    PUSH(4,6,3,2,pFV,pS2E); /// {0,1/6} TP=0 FP=0 FN=2 TN=3 Confl=3*2/2+3*(4/2)+2*(1/2+2+3+4)=28
+    PUSH(7,13,2,3,pFV,pS2E); /// {5/6,1} TP=3 FP=2 FN=0 TN=0 Confl=2*3/2+2*(4+3+2+1/2+2)+3*(4/2)=32
+    PUSH(14.5,0.5,1,4,pFV,pS2E); /// {0,1/3} TP=0 FP=0 FN=4 TN=1 Confl=1*4/2+1*(4/2+3/2+2/2)+4*(1/2+2/2+3+4+3/2+2)=54.5
+    PUSH(-1,16,2,3,pFV,pS2E); /// {0,5/6} TP=1.5 FP=1 FN=1.5 TN=1 Confl=2*3/2+2*(4/2+3/2+2/2+1/2+2/2+3/2+4/2)+3*(1/2+2/2+3/2+4/2+3/2+2/2+1/2)=46
+    PUSH(-1,18,3,2,pFV,pS2E); /// {0,1} TP=1 FP=1.5 FN=1 TN=1.5 Confl=3*2/2+3*(4/2+3/2+2/2+1/2+2/2+3/2+4/2+3/2)+2*(1/2+2/2+3/2+4/2+3/2+2/2+1/2+2/2)=54
+    PUSH(12.5,1.5,4,1,pFV,pS2E); /// {1/6,1/3} TP=0 FP=0 FN=1 TN=4 Confl=4*1/2+4*(4/2+3/2+2/2+4/2+3/2+2/2)+1*(1/2+2/2+3+4+3/2+2+1/2+2/2+3/2)=53
+    PUSH(4,8,3,2,pFV,pS2E); /// {1/6,2/3} TP=1 FP=1.5 FN=1 TN=1.5 Confl=3*2/2+3*(4/2+3/2+2/2+2/2+4/2+3/2+2/2+1/2)+2*(1/2+2/2+3/2+4+3/2+2+1/2+2/2+3/2+4/2)=65.5
+    PUSH(3,13,2,3,pFV,pS2E); /// {1/6,5/6} TP=1.5 FP=1 FN=1.5 TN=1 Confl=2*3/2+2*(4+3+2+1+2+3+4+3+2+1+2)/2+3*(1+2+3+4+3+2+1+2+3+4+3)/2=72
+    PUSH(1,18,1,4,pFV,pS2E); /// {1/6,1} TP=2 FP=0.5 FN=2 TN=0.5 Confl=1*4/2+1*(4+3+2+1+2+3+4+3+2+1+2+3)/2+4*(1+2+3+4+3+2+1+2+3+4+3+2)/2=77
+    PUSH(15,5.5,2,3,pFV,pS2E); /// {1/3,5/6} TP=1.5 FP=1 FN=1.5 TN=1 Confl=2*3/2+2*(2*4+3+2+1+2*2+3+4+3+2+1+2+3+4)/2+3*(2+3+4+2+1+2+3+4+3+2+1)/2=83.5
+    PUSH(18,1,3,2,pFV,pS2E); /// {1/3,1} TP=1 FP=1.5 FN=1 TN=1.5 Confl=3*2/2+3*(2*4+3+2+1+2*2+3+4+3+2+1+2+3+4+3)/2+2*(2+3+4+2+1+2+3+4+3+2+1+2)/2=96.5
+    PUSH(8,12,4,1,pFV,pS2E); /// {2/3,5/6} TP=1 FP=4 FN=0 TN=0 Confl=4*1/2+4*(4+3+2/2+1/2+2+3/2+4+3/2+2/2+1+2/2+3/2+4/2+3/2+2/2)+1*(3/2+4/2+2/2+2/2+3/2+3/2+2/2+1/2+2/2+3/2)=120.5
 
     auto pEvaluator = std::make_shared<Evaluator>();
-    auto pSample2Evaluate = std::make_shared<Sample>(2);
-    (*pEvaluator).set_border_system(pBS).set_sample(pSample2Evaluate);
-    pSample2Evaluate->push((*pSample)[0]);
+    (*pEvaluator).set_sample(pS2E).set_border_system(pBS);
+
+    std::pair<std::pair<double, double>, std::pair<double, double>> confusion_matrix({{16.5,21}, {23.5,19}});
+    BOOST_CHECK(pEvaluator->get_confusion_matrix() == confusion_matrix);
+    BOOST_CHECK_EQUAL(pEvaluator->get_error_rate(), (21.0+23.5)/80.0);
+    BOOST_CHECK_EQUAL(pEvaluator->get_accuracy(), (16.5+19.0)/80.0);
+
+    const double num_conflicts = 2+11+24+38+28+32+54.5+46+54+53+65.5+72+77+83.5+96.5+120.5;
+    double neg, pos;
+    std::tie(neg,pos) = pS2E->get_neg_pos_counts();
+
+    BOOST_CHECK_EQUAL(pEvaluator->get_ranking_conflicts(), num_conflicts);
+    BOOST_CHECK_EQUAL(pEvaluator->get_roc_error(), num_conflicts/(neg*pos));
+    BOOST_CHECK_EQUAL(pEvaluator->get_roc(), 1-num_conflicts/(neg*pos));
 }
--- lib/evaluator.h
+++ lib/evaluator.h
@@ -12,22 +12,129 @@ public:
     Evaluator & set_sample(const std::shared_ptr<Sample> &);
     Evaluator & set_border_system(const std::shared_ptr<BorderSystem> &);
 
+    /// { {TP,FP}, {FN,TN} }
+    std::pair<std::pair<double, double>, std::pair<double, double>> get_confusion_matrix();
+    double get_error_rate(); /// 1 - accuracy
+    double get_accuracy();
+    double get_ranking_conflicts();
+    double get_roc_error(); /// 1 - roc
+    double get_roc();
+
 private:
     std::shared_ptr<Sample> pSample_;
     std::shared_ptr<BorderSystem> pBorderSystem_;
+    bool confusion_matrix_computed_;
+    std::pair<std::pair<double, double>, std::pair<double, double>> confusion_matrix_;
+    bool ranking_conflicts_computed_;
+    double ranking_conflicts_;
+
+    void compute_confusion_matrix();
+    void compute_ranking_conflicts();
 };
 
-Evaluator::Evaluator() : pSample_(nullptr), pBorderSystem_(nullptr) {
-}
+Evaluator::Evaluator() : pSample_(nullptr), pBorderSystem_(nullptr),
+                         confusion_matrix_computed_(false), confusion_matrix_({{0,0},{0,0}}),
+                         ranking_conflicts_computed_(false), ranking_conflicts_(0)
+{}
 
 Evaluator &Evaluator::set_sample(const std::shared_ptr<Sample> & pSample) {
     pSample_ = pSample;
+    confusion_matrix_computed_ = false;
+    ranking_conflicts_computed_ = false;
+    confusion_matrix_ = std::pair<std::pair<double, double>, std::pair<double, double>> ({{0,0},{0,0}});
+    ranking_conflicts_ = 0;
     return *this;
 }
 
 Evaluator &Evaluator::set_border_system(const std::shared_ptr<BorderSystem> & pBorderSystem) {
     pBorderSystem_ = pBorderSystem;
+    confusion_matrix_computed_ = false;
+    ranking_conflicts_computed_ = false;
+    confusion_matrix_ = std::pair<std::pair<double, double>, std::pair<double, double>> ({{0,0},{0,0}});
+    ranking_conflicts_ = 0;
     return *this;
+}
+
+void Evaluator::compute_confusion_matrix() {
+    const unsigned int size = pSample_->size();
+    confusion_matrix_.first.first = confusion_matrix_.first.second =
+            confusion_matrix_.second.first = confusion_matrix_.second.second = 0;
+    double c1=0, c2=0;
+    for ( unsigned int i = 0; i < size; i++ ) {
+        const auto & data = (*pSample_)[i]->get_data();
+        std::tie(c1,c2) = pBorderSystem_->confidence_interval(data);
+        if (c1 > 0.5) {
+            confusion_matrix_.first.first += (*pSample_)[i]->get_weight_positives();
+            confusion_matrix_.first.second += (*pSample_)[i]->get_weight_negatives();
+        } else if (c2 < 0.5) {
+            confusion_matrix_.second.first += (*pSample_)[i]->get_weight_positives();
+            confusion_matrix_.second.second += (*pSample_)[i]->get_weight_negatives();
+        } else {
+            confusion_matrix_.first.first += (*pSample_)[i]->get_weight_positives()/2;
+            confusion_matrix_.first.second += (*pSample_)[i]->get_weight_negatives()/2;
+            confusion_matrix_.second.first += (*pSample_)[i]->get_weight_positives()/2;
+            confusion_matrix_.second.second += (*pSample_)[i]->get_weight_negatives()/2;
+        }
+    }
+    confusion_matrix_computed_ = true;
+}
+
+void Evaluator::compute_ranking_conflicts() {
+    ranking_conflicts_ = 0;
+    double c1=0, c2=0, d1=0, d2=0;
+    const unsigned int size = pSample_->size();
+    for ( unsigned int i = 0; i < size; i++ ) {
+        std::tie(c1,c2) = pBorderSystem_->confidence_interval((*pSample_)[i]->get_data());
+        for ( unsigned int j = 0; j < size; j++ ) {
+            std::tie(d1,d2) = pBorderSystem_->confidence_interval((*pSample_)[j]->get_data());
+            if (d1>c2) {
+                ranking_conflicts_ += (*pSample_)[j]->get_weight_negatives() * (*pSample_)[i]->get_weight_positives();
+            } else if (d2<c1) {
+                ranking_conflicts_ += (*pSample_)[i]->get_weight_negatives() * (*pSample_)[j]->get_weight_positives();
+            } else {
+                ranking_conflicts_ += (
+                        (*pSample_)[j]->get_weight_negatives() * (*pSample_)[i]->get_weight_positives()
+                        + (*pSample_)[i]->get_weight_negatives() * (*pSample_)[j]->get_weight_positives()
+                )/2;
+            }
+        }
+    }
+    ranking_conflicts_ /= 2;
+    ranking_conflicts_computed_ = true;
+}
+
+std::pair<std::pair<double, double>, std::pair<double, double>> Evaluator::get_confusion_matrix() {
+    if (!confusion_matrix_computed_) compute_confusion_matrix();
+    return confusion_matrix_;
+}
+
+double Evaluator::get_error_rate() {
+    if (!confusion_matrix_computed_) compute_confusion_matrix();
+    double const f = confusion_matrix_.first.second + confusion_matrix_.second.first;
+    double const all = f +confusion_matrix_.first.first + confusion_matrix_.second.second;
+    if (all == 0) return 0;
+    return f / all;
+}
+
+double Evaluator::get_accuracy() {
+    return 1 - get_error_rate();
+}
+
+double Evaluator::get_ranking_conflicts() {
+    if (!ranking_conflicts_computed_) compute_ranking_conflicts();
+    return ranking_conflicts_;
+}
+
+double Evaluator::get_roc_error() {
+    double n=0, p=0;
+    std::tie(n,p) = pSample_->get_neg_pos_counts();
+    const double possible_conflicts = n*p;
+    if (possible_conflicts == 0) return 0;
+    return get_ranking_conflicts()/possible_conflicts;
+}
+
+double Evaluator::get_roc() {
+    return 1-get_roc_error();
 }
 
 #endif
