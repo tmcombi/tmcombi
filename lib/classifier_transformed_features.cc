89cd6e61e8eab4fabc16a5906460016c2aee2c1c
Sergey Grosman <srg.grosman@gmail.com>
2020-12-25 00:02:09 +0100

    update

--- lib/classifier_transformed_features.cc
+++ lib/classifier_transformed_features.cc
@@ -2,8 +2,10 @@
 #include <boost/test/included/unit_test.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
+#include "classifier_transformed_features.h"
 #include "feature_names.h"
 #include "train_classifier_tmc.h"
+#include "feature_transform_subset.h"
 
 BOOST_AUTO_TEST_CASE( classifier_transformed_features_basics )
 {
@@ -18,46 +20,52 @@ BOOST_AUTO_TEST_CASE( classifier_transformed_features_basics )
 
     std::shared_ptr<Sample> pSample = sample_creator.from_file(data_file);
 
-    BOOST_TEST_MESSAGE("Resulting sample: " << *pSample);
+    BOOST_TEST_MESSAGE("Original sample: " << *pSample);
     BOOST_CHECK_EQUAL(pSample->dim(), 2);
     BOOST_CHECK_EQUAL(pSample->size(), 36);
 
-    std::shared_ptr<TrainClassifier> pTC = std::make_shared<TrainClassifierTmc>(pSample);
+    auto pFT = std::make_shared<FeatureTransformSubset>(boost::dynamic_bitset<>(std::string("10")));
+    std::shared_ptr<Sample> pSampleTransformed = std::make_shared<Sample>(1);
+    for ( size_t i = 0; i < pSample->size(); i++) {
+        const auto pFV = pFT->transform_feature_vector((*pSample)[i]);
+        pSampleTransformed->push(pFV);
+    }
+    BOOST_TEST_MESSAGE("Transformed sample: " << *pSampleTransformed);
+    BOOST_CHECK_EQUAL(pSampleTransformed->dim(), 1);
+    BOOST_CHECK_EQUAL(pSampleTransformed->size(), 17);
+
+    std::shared_ptr<TrainClassifier> pTC = std::make_shared<TrainClassifierTmc>(pSampleTransformed);
     pTC->train();
 
     std::shared_ptr<Classifier> pClTmc = pTC->get_classifier();
 
-    std::vector<double> p;  std::pair<double, double> conf;
-    p = {10,2}; conf = {1.0/6.0,1.0/6.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), conf.first);
-    p = {16,1}; conf = {1.0/3.0,1.0/3.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), conf.first);
-    p = {7,10}; conf = {2.0/3.0,2.0/3.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), conf.first);
-    p = {11,10}; conf = {5.0/6.0,5.0/6.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), conf.first);
-    p = {4,6}; conf = {0,1.0/6.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), conf.second);
-    p = {7,13}; conf = {5.0/6.0,1}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), conf.first);
-    p = {14.5,0.5}; conf = {0,1.0/3.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (10+8)/(double)(50+10+16+8));
-    p = {-1,16}; conf = {0,5.0/6.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (10+8+18+45)/(double)(50+10+16+8+9+18+9+45));
-    p = {-1,18}; conf = {0,1}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (10+8+18+45)/(double)(50+10+16+8+9+18+9+45));
-    p = {12.5,1.5}; conf = {1.0/6.0,1.0/3.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (10+8)/(double)(50+10+16+8));
-    p = {4,8}; conf = {1.0/6.0,2.0/3.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (10+8+18)/(double)(50+10+16+8+9+18));
-    p = {3,13}; conf = {1.0/6.0,5.0/6.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (10+8+18+45)/(double)(50+10+16+8+9+18+9+45));
-    p = {1,18};  conf = {1.0/6.0,1}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (10+8+18+45)/(double)(50+10+16+8+9+18+9+45));
-    p = {15,5.5}; conf = {1.0/3.0,5.0/6.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (8+18+45)/(double)(16+8+9+18+9+45));
-    p = {18,1}; conf = {1.0/3.0,1}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (8+18+45)/(double)(16+8+9+18+9+45));
-    p = {8,12}; conf = {2.0/3.0,5.0/6.0}; BOOST_CHECK(pClTmc->confidence_interval(p) == conf);
-    BOOST_CHECK_EQUAL(pClTmc->confidence(p), (18+45)/(double)(9+18+9+45));
+    BOOST_CHECK_EQUAL(pClTmc->confidence({0}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({1}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({2}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({3}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({4}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({5}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({6}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({7}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({8}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({9}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({10}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({11}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTmc->confidence({12}), 0.63492063492063489);
+
+    std::shared_ptr<Classifier> pClTrF = std::make_shared<ClassifierTransformedFeatures>(pClTmc,pFT);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,0}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,1}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,2}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,3}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,4}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,5}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,6}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,7}), 0.32911392405063289);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,8}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,9}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,10}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,11}), 0.63492063492063489);
+    BOOST_CHECK_EQUAL(pClTrF->confidence({123,12}), 0.63492063492063489);
+
 }
--- lib/classifier_transformed_features.h
+++ lib/classifier_transformed_features.h
@@ -4,12 +4,46 @@
 #include <utility>
 
 #include "classifier.h"
+#include "feature_transform.h"
 
 class ClassifierTransformedFeatures : public Classifier {
 public:
+    ClassifierTransformedFeatures( std::shared_ptr<const Classifier> ,
+            std::shared_ptr<const FeatureTransform> );
+    double confidence(const std::vector<double> &) const override;
+    std::pair<double,double> confidence_interval(const std::vector<double> &) const override;
+    std::pair<double,std::pair<double,double>>
+    confidence_and_confidence_interval(const std::vector<double> &) const override;
 
 private:
+    std::shared_ptr<const Classifier> pClassifier_;
+    std::shared_ptr<const FeatureTransform> pFT_;
 
 };
+
+ClassifierTransformedFeatures::ClassifierTransformedFeatures(std::shared_ptr<const Classifier>  pClassifier,
+                                                             std::shared_ptr<const FeatureTransform> pFT) :
+        pClassifier_(std::move(pClassifier)), pFT_(std::move(pFT)) {
+}
+
+double ClassifierTransformedFeatures::confidence(const std::vector<double> & v) const {
+    std::vector<double> vt;
+    pFT_->transform_std_vector(v,vt);
+    return pClassifier_->confidence(vt);
+}
+
+std::pair<double, double> ClassifierTransformedFeatures::confidence_interval(const std::vector<double> & v) const {
+    std::vector<double> vt;
+    pFT_->transform_std_vector(v,vt);
+    return pClassifier_->confidence_interval(vt);
+}
+
+std::pair<double, std::pair<double, double>>
+ClassifierTransformedFeatures::confidence_and_confidence_interval(const std::vector<double> & v) const {
+    std::vector<double> vt;
+    pFT_->transform_std_vector(v,vt);
+    return pClassifier_->confidence_and_confidence_interval(vt);
+}
+
 
 #endif
