60e673faf8e06c14006aad430bde6201b618bff7
Sergey Grosman <srg.grosman@gmail.com>
2020-12-29 01:22:59 +0100

    implement dispatch_ptree

--- lib/classifier_creator_dispatch_ptree.h
+++ lib/classifier_creator_dispatch_ptree.h
@@ -2,6 +2,9 @@
 #define LIB_CLASSIFIER_CREATOR_DISPATCH_PTREE_H_
 
 #include "classifier_creator.h"
+#include "classifier_tmc.h"
+#include "classifier_transformed_features.h"
+#include "feature_transform_subset.h"
 
 class ClassifierCreatorDispatchPtree : public ClassifierCreator {
 public:
@@ -18,9 +21,11 @@ private:
 ClassifierCreatorDispatchPtree::ClassifierCreatorDispatchPtree(const boost::property_tree::ptree & pt) : pC_(nullptr) {
     auto type = pt.get<std::string>("type");
     if ( type == "ClassifierTmc" ) {
-        pC_ = std::make_shared<ClassifierTmc>(std::make_shared<BorderSystem>(pt));
-    } else if ( false ) {
-
+        pC_ = std::make_shared<ClassifierTmc>( std::make_shared<BorderSystem>(pt) );
+    } else if ( type == "ClassifierTransformedFeatures" ) {
+        const std::shared_ptr<ClassifierCreator> pCC = std::make_shared<ClassifierCreatorDispatchPtree>( pt.get_child("classifier") );
+        const std::shared_ptr<FeatureTransform> pFT = std::make_shared<FeatureTransformSubset>( pt.get_child("feature_transform") );
+        pC_ = std::make_shared<ClassifierTransformedFeatures>(pCC->get_classifier(),pFT);
     } else {
         throw std::runtime_error("Classifier configuration type is not supported");
     }
