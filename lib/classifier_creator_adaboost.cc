8bdd302cfa386a927195c4efb2a0c82418bb1d03
Sergey Grosman <srg.grosman@gmail.com>
2021-02-09 01:50:15 +0100

    adaboost4

--- lib/classifier_creator_adaboost.cc
+++ lib/classifier_creator_adaboost.cc
@@ -4,8 +4,10 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include "feature_names.h"
-#include "classifier_creator_train_tmc.h"
 #include "classifier_creator_adaboost.h"
+#include "classifier_creator_fs_graph.h"
+#include "classifier_creator_fs_n_fold.h"
+#include "classifier_creator_train_tmc.h"
 
 //BOOST_AUTO_TEST_SUITE( classifier_creator_adaboost )
 
@@ -102,6 +104,77 @@ BOOST_AUTO_TEST_CASE( classifier_creator_tmc_1k )
     BOOST_CHECK_EQUAL(pSampleEval->size(), 992);
 
     std::shared_ptr<ClassifierCreatorTrain> pCC_aux = std::make_shared<ClassifierCreatorTrainTmc>();
+    std::shared_ptr<ClassifierCreatorAdaboost> pCCAdaBoost = std::make_shared<ClassifierCreatorAdaboost>();
+    pCCAdaBoost->verbose(true);
+    (*pCCAdaBoost).set_classifier_creator_train(pCC_aux).
+            init(pSample).
+            set_eval_sample(pSampleEval).
+            set_trials(16).
+            train();
+
+    std::shared_ptr<Classifier> pTmcClBoosted = pCCAdaBoost->get_classifier();
+}
+
+
+BOOST_AUTO_TEST_CASE( classifier_creator_fs_graph_tmc_1k )
+{
+    const std::string names_file("data/tmc_paper/tmc_paper.names");
+    const std::string data_file("data/tmc_paper/tmc_paper.data");
+    const std::string eval_file("data/tmc_paper/tmc_paper.test");
+    BOOST_TEST_MESSAGE("Creating train sample from file: " << data_file);
+    BOOST_TEST_MESSAGE("Creating evaluation sample from file: " << eval_file);
+
+    std::shared_ptr<FeatureNames> pFN = std::make_shared<FeatureNames>(names_file);
+
+
+    SampleCreator sample_creator;
+    sample_creator.set_feature_names(pFN);
+
+    std::shared_ptr<Sample> pSample = sample_creator.from_file(data_file);
+    std::shared_ptr<Sample> pSampleEval = sample_creator.from_file(eval_file);
+
+    BOOST_CHECK_EQUAL(pSample->dim(), 2);
+    BOOST_CHECK_EQUAL(pSample->size(), 992);
+    BOOST_CHECK_EQUAL(pSampleEval->size(), 992);
+
+    std::shared_ptr<ClassifierCreatorTrain> pCC_aux_tmc = std::make_shared<ClassifierCreatorTrainTmc>();
+    std::shared_ptr<ClassifierCreatorFsGraph> pCC_aux = std::make_shared<ClassifierCreatorFsGraph>();
+    pCC_aux->set_classifier_creator_train(pCC_aux_tmc);
+    std::shared_ptr<ClassifierCreatorAdaboost> pCCAdaBoost = std::make_shared<ClassifierCreatorAdaboost>();
+    pCCAdaBoost->verbose(true);
+    (*pCCAdaBoost).set_classifier_creator_train(pCC_aux).
+            init(pSample).
+            set_eval_sample(pSampleEval).
+            set_trials(16).
+            train();
+
+    std::shared_ptr<Classifier> pTmcClBoosted = pCCAdaBoost->get_classifier();
+}
+
+BOOST_AUTO_TEST_CASE( classifier_creator_fs_n_fold_tmc_1k )
+{
+    const std::string names_file("data/tmc_paper/tmc_paper.names");
+    const std::string data_file("data/tmc_paper/tmc_paper.data");
+    const std::string eval_file("data/tmc_paper/tmc_paper.test");
+    BOOST_TEST_MESSAGE("Creating train sample from file: " << data_file);
+    BOOST_TEST_MESSAGE("Creating evaluation sample from file: " << eval_file);
+
+    std::shared_ptr<FeatureNames> pFN = std::make_shared<FeatureNames>(names_file);
+
+
+    SampleCreator sample_creator;
+    sample_creator.set_feature_names(pFN);
+
+    std::shared_ptr<Sample> pSample = sample_creator.from_file(data_file);
+    std::shared_ptr<Sample> pSampleEval = sample_creator.from_file(eval_file);
+
+    BOOST_CHECK_EQUAL(pSample->dim(), 2);
+    BOOST_CHECK_EQUAL(pSample->size(), 992);
+    BOOST_CHECK_EQUAL(pSampleEval->size(), 992);
+
+    std::shared_ptr<ClassifierCreatorTrain> pCC_aux_tmc = std::make_shared<ClassifierCreatorTrainTmc>();
+    std::shared_ptr<ClassifierCreatorFsNfold> pCC_aux = std::make_shared<ClassifierCreatorFsNfold>();
+    pCC_aux->set_classifier_creator_train(pCC_aux_tmc);
     std::shared_ptr<ClassifierCreatorAdaboost> pCCAdaBoost = std::make_shared<ClassifierCreatorAdaboost>();
     pCCAdaBoost->verbose(true);
     (*pCCAdaBoost).set_classifier_creator_train(pCC_aux).
